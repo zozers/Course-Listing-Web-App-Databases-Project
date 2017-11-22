2ae487b4ed6d07e9f6803561f7a5137eb11e33b9
Zoe Offermann <zoffermann15>
2017-11-22 13:44:35 -0800

    updated schema to look more like our final E/R diagram

--- Schema.sql
+++ Schema.sql
@@ -1,21 +1,46 @@
 CREATE SCHEMA course_guide_data;
 
-drop table Teachers;
-drop table Departments;
-drop table Classrooms;
-drop table Course_Offerings;
-drop table Course;
-drop table Teach;
-drop table ISA;
-drop table Part_Of;
-drop table Taught_In;
+drop table professor CASCADE;
+drop table departments CASCADE;
+drop table classrooms CASCADE;
+drop table offering CASCADE;
+drop table course CASCADE;
+drop table weekday CASCADE;
+drop table building CASCADE;
+drop table teaches CASCADE;
+drop table IMPLEMENTS CASCADE;
+drop table Offers CASCADE;
+drop table Taught_In CASCADE;
+drop table Located_In CASCADE;
+drop table Offered_ON CASCADE;
+drop table Requires CASCADE;
 
 
 
 
-create table Teachers(
+-- department(name)
+-- professor(name, id, email)
+-- course(name, description, id, credits, first_year_friendly, first_semester_offered)
+-- offering(id, semester, pf, max_students)
+-- classroom(code, capacity)
+-- weekday(name)
+-- building(name) 
+
+-- teaches(professor.id, offering.id, offering.semester) 
+-- taughtIn(offering.id, offering.semester, classroom.code)
+-- locatedIn(classroom.code, building.name)
+-- implements(offering.id, offering.semester, course.id,)
+-- offers(department.name, course.id)
+-- offeredOn(offering.id, offering.semester, weekday.name, time_start, time_end)
+-- requires(course.id, course.id, coreq)
+
+
+
+create table Professor(
    NAME VARCHAR (20)     NOT NULL,
-   PRIMARY KEY (NAME)
+   ID int   NOT NULL,
+   EMAIL VARCHAR(50)    NOT NULL,
+   PRIMARY KEY (ID)
 );
 
 
@@ -24,56 +49,88 @@ create table Departments(
    PRIMARY KEY (NAME)
 );
 
-create table Classrooms(
+create table Building(
    NAME VARCHAR (20)     NOT NULL,
-   BUILDING VARCHAR (20)     NOT NULL,
    PRIMARY KEY (NAME)
 );
 
-create table Course_Offerings(
+create table Weekday(
    NAME VARCHAR (20)     NOT NULL,
-   CREDIT VARCHAR (20)     NOT NULL,
-   DESCRIPTION   VARCHAR(800)   NOT NULL,
-   Size INT 	NOT NULL,
-   FIRST_YEAR VARCHAR(20) 	NOT NULL,
    PRIMARY KEY (NAME)
+);
+
+create table Classrooms(
+   CODE VARCHAR (20)     NOT NULL,
+   CAPACITY int     NOT NULL,
+   PRIMARY KEY (CODE)
 );
 
 create table Course(
+   NAME VARCHAR (20)     NOT NULL,
+   CREDIT VARCHAR (20)     NOT NULL,
+   DESCRIPTION   VARCHAR(800)   NOT NULL,
+   FIRST_YEAR VARCHAR(20) 	NOT NULL,
+   NEW VARCHAR(10)   NOT NULL,
+   ID VARCHAR(10)    NOT NULL,
+
+   PRIMARY KEY (ID)
+);
+
+create table Offering(
    ID VARCHAR (20)      NOT NULL ,
-   MEET_TIME VARCHAR (20)     NOT NULL,
-   YEAR INT 	NOT NULL,
-   PRIMARY KEY (ID, YEAR)
+   SEMESTER VARCHAR (10)     NOT NULL,
+   PF VARCHAR(10) 	NOT NULL,
+   MAX_STUDENTS INT  NOT NULL,
+   PRIMARY KEY (ID, SEMESTER)
 );
 
-create table Teach(
-   CID VARCHAR,
-   CYEAR INT,
-   TNAME VARCHAR REFERENCES Teachers(NAME),
-   FOREIGN KEY (CID, CYEAR) REFERENCES Course (ID, YEAR)
+create table Teaches(
+   OfID VARCHAR,
+   OFSEMESTER VARCHAR,
+   TID INT REFERENCES Professor(ID),
+   FOREIGN KEY (OfID, OFSEMESTER) REFERENCES Offering (ID, SEMESTER)
 );
 
 
-create table Part_Of(
-   CNAME VARCHAR,
+create table Offers(
+   CfID VARCHAR,
    DNAME VARCHAR REFERENCES Departments(NAME),
-   FOREIGN KEY (CNAME) REFERENCES Course_Offerings(NAME)
+   FOREIGN KEY (CfID) REFERENCES Course (ID)
 );
 
-create table ISA(
-   CID VARCHAR,
-   CYEAR INT,
-   CNAME VARCHAR REFERENCES Course_Offerings(NAME),
-   FOREIGN KEY (CID, CYEAR) REFERENCES Course (ID, YEAR)
+create table IMPLEMENTS(
+   OFID VARCHAR,
+   OFSEMESTER VARCHAR,
+   CFID VARCHAR REFERENCES Course(ID),
+   FOREIGN KEY (OFID, OFSEMESTER) REFERENCES Offering(ID, SEMESTER)
 
 );
 
 
 create table Taught_In(
-   CID VARCHAR,
-   CYEAR INT,
-   FOREIGN KEY (CID, CYEAR) REFERENCES Course (ID, YEAR),
-   RNAME VARCHAR REFERENCES Classrooms(NAME)
+   OFID VARCHAR,
+   OFSEMESTER VARCHAR,
+   FOREIGN KEY (OFID, OFSEMESTER) REFERENCES Offering (ID, SEMESTER),
+   RCODE VARCHAR REFERENCES Classrooms(CODE)
+);
+
+create table Located_In(
+   Bname VARCHAR REFERENCES Building(NAME),
+   RCODE VARCHAR REFERENCES Classrooms(CODE)
+);
+
+create table Offered_ON(
+   OFID VARCHAR,
+   OFSEMESTER VARCHAR,
+   DAY VARCHAR REFERENCES Weekday(NAME),
+   Time_start TIME,
+   Time_end TIME,
+   FOREIGN KEY (OFID, OFSEMESTER) REFERENCES Offering (ID, SEMESTER)
+);
+
+create table Requires(
+   CID VARCHAR REFERENCES Course (ID),
+   RID VARCHAR REFERENCES Course(ID)
 );
 
 
